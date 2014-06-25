f6a422c4bc10745bdd863ada96b3040e2cdbaa64
Thomas Scheibitz <thomas.scheibitz@in2code.de>
2014-06-25 08:07:30 +0200

    add Controller

---
+++ Classes/Controller/AdressmapController.php
@@ -0,0 +1,63 @@
+<?php
+namespace Kreativschmiede\Runninglist\Controller;
+
+
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2014 Thomas Scheibitz <mail@kreativschmiede-eichsfeld.de>, Kreativschmiede Eichsfeld
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * RunnerController
+ */
+class RunnerController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+
+	/**
+	 * runnerRepository
+	 *
+	 * @var \Kreativschmiede\Runninglist\Domain\Repository\RunnerRepository
+	 * @inject
+	 */
+	protected $runnerRepository = NULL;
+
+	/**
+	 * action list
+	 *
+	 * @return void
+	 */
+	public function listAction() {
+		$runners = $this->runnerRepository->findAll();
+		$this->view->assign('runners', $runners);
+	}
+
+	/**
+	 * action show
+	 *
+	 * @param \Kreativschmiede\Runninglist\Domain\Model\Runner $runner
+	 * @return void
+	 */
+	public function showAction(\Kreativschmiede\Runninglist\Domain\Model\Runner $runner) {
+		$this->view->assign('runner', $runner);
+	}
+
+}
