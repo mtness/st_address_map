81de070faaa9091f61d43740c6b3c225bcb2b80d
Thomas Scheibitz <thomas.scheibitz@in2code.de>
2014-06-26 12:11:15 +0200

    set Version 0.3.6

--- pi1/class.tx_staddressmap_pi1.php
+++ pi1/class.tx_staddressmap_pi1.php
@@ -293,7 +293,7 @@ class tx_staddressmap_pi1 extends tslib_pibase {
 				$res = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
 					'uid, ' . (!empty($validDatabaseFields) ? implode(', ', $validDatabaseFields) . ', ' : '') . 'tx_staddressmap_lat, tx_staddressmap_lng',
 					'tt_address',
-					'(hidden=0 and deleted=0) and (pid = ' . $addresslist . ') and ' . $what . ' like ' . $GLOBALS['TYPO3_DB']->escapeStrForLike($var, 'tt_address')
+					'(hidden=0 and deleted=0) and (pid = ' . $addresslist . ') and ' . $what . ' like "' . $GLOBALS['TYPO3_DB']->escapeStrForLike($var, 'tt_address') . '"'
 				);
 			}
 		}
