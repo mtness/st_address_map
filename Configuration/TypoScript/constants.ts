e56e4cdd11db9e066394a1f70e17299a5b89ff27
Thomas Scheibitz <thomas.scheibitz@in2code.de>
2016-12-13 09:27:07 +0100

    [TASK] set correctly path

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -1,5 +1,5 @@
 PATH {
-	JAVASCRIPTS = /typo3conf/ext/st_address_map/Resources/Public/JavaScripts
+	JAVASCRIPTS = /typo3conf/ext/st_address_map/Resources/Public/Javascripts
 }
 
 plugin.tx_staddressmap {
